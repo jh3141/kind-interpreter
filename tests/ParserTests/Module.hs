f2f2d35907d64810a9b50bdf7687c18aefa9524c
Julian Hall <jh314159@googlemail.com>
2016-03-16 16:05:07 +0000

    Additional unit tests for module-level parsing

--- tests/ParserTests/Module.hs
+++ tests/ParserTests/Module.hs
@@ -7,13 +7,33 @@ import KindLang.Data.AST
 import Text.Parsec
     
 parseString :: Parsec String () r -> String -> r
-parseString parser string = case parse parser "test" string of
-                              Right x -> x
-                              Left  e -> error $ show e
-                                    
+parseString parser toParse = case parse parser "test" toParse of
+                               Right x -> x
+                               Left  e -> error $ show e
+parseMod :: String -> Module
+parseMod = parseString _module_
+           
 moduleParserTests :: TestTree
 moduleParserTests =
     testGroup "Module parser" [
         testCase "empty module" $
-                 parseString _module_ "" @=? Module Nothing [] []      
+                 parseMod "" @=? Module Nothing [] [],
+        testCase "module line sets name" $
+                 parseMod "module ModuleName;" @=?
+                          Module (Just $ UnqualifiedID "ModuleName") [] [],
+        testCase "module name can be scoped" $
+                 parseMod "module ModuleName::SubName::SubSubName;" @=?
+                          Module (Just $ QualifiedID "ModuleName"
+                                       $ QualifiedID "SubName"
+                                       $ UnqualifiedID "SubSubName")
+                                 [] [],
+        testCase "import declaration parsed" $
+                 parseMod "import id1::id2;" @=?
+                          Module Nothing [UnqualifiedModuleImport
+                                              (QualifiedID "id1" $
+                                               UnqualifiedID "id2") False] [],
+        testCase "whole-namespace import declaration parsed" $
+                 parseMod "import id1::*;" @=?
+                          Module Nothing [UnqualifiedModuleImport
+                                              (UnqualifiedID "id1") True] []
     ]                     
