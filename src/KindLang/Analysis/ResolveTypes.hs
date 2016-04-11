c4791dd6d66dff303b8630f95a8b016d23906d84
Julian Hall <jh314159@googlemail.com>
2016-04-11 21:15:37 +0100

    Resolve types for class member variables

--- src/KindLang/Analysis/ResolveTypes.hs
+++ src/KindLang/Analysis/ResolveTypes.hs
@@ -6,14 +6,26 @@ import KindLang.Data.Catalogue
     
  
 resolveTypes :: Module -> Module
-resolveTypes mod = mod
+resolveTypes m = m
                    
-resolveDeflistTypes :: Catalogue -> DefList -> DefList
-resolveDeflistTypes cat = map (second $ resolveDefinition cat)
+resolveDefListTypes :: Catalogue -> DefList -> DefList
+resolveDefListTypes cat = map (second $ resolveDefinition cat)
+
+resolveClassDefListTypes :: Catalogue -> [ClassMember] -> [ClassMember]
+resolveClassDefListTypes cat = map (resolveClassMember cat)
+resolveClassMember :: Catalogue -> ClassMember -> ClassMember
+resolveClassMember cat (ClassMember n v d) = ClassMember n v
+                                             (resolveDefinition cat d)
 
 resolveDefinition :: Catalogue -> Definition -> Definition
 resolveDefinition cat (VariableDefinition (SimpleType sid) i) =
     case lookupHierarchical cat sid of
       Left err -> error $ show err -- FIXME
       Right (cid, def) -> VariableDefinition (ResolvedType sid cid def) i
-resolveDefinition cat nonMatching = nonMatching
+                          
+resolveDefinition cat (ClassDefinition cdlist) =
+    ClassDefinition $ resolveClassDefListTypes cat cdlist
+                    
+resolveDefinition _ nonMatching = nonMatching
+
+                                    
--- tests/AnalysisTests/TypeResolution.hs
+++ tests/AnalysisTests/TypeResolution.hs
@@ -12,7 +12,7 @@ typeResolutionTests =
     testGroup "Type resolution"
     [
         testCase "resolve module-level variable with identified type" $
-                 (resolveDeflistTypes testCatalogue
+                 (resolveDefListTypes testCatalogue
                   [("v", (VariableDefinition
                           (SimpleType simpleClass)
                           VarInitNone))]) @?=
@@ -20,13 +20,24 @@ typeResolutionTests =
                          (ResolvedType simpleClass simpleClass def)
                          VarInitNone))],
         testCase "resolve variable with renamed type" $
-                 (resolveDeflistTypes testCatalogue
+                 (resolveDefListTypes testCatalogue
                   [("v", (VariableDefinition
                           (SimpleType renamedClass)
                           VarInitNone))]) @?=
                  [("v", (VariableDefinition
                          (ResolvedType renamedClass originalClass def)
-                         VarInitNone))]
+                         VarInitNone))],
+        testCase "resolve class member variables" $
+                 (resolveDefListTypes testCatalogue
+                  [("C", (ClassDefinition [
+                           (ClassMember "v" Public
+                            (VariableDefinition (SimpleType simpleClass)
+                                                VarInitNone))]))]) @?=
+                 [("C", (ClassDefinition [
+                          (ClassMember "v" Public
+                           (VariableDefinition
+                            (ResolvedType simpleClass simpleClass def)
+                            VarInitNone))]))]
     ]
 
 simpleClass :: ScopedID
